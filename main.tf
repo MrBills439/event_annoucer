fdadac5cdbe54bb01b36b645d4559ca5779b8fc7
Felix Appiah-Kubi <feddybills17@outlook.com>
2025-10-07 16:23:11 +0100

    aws event annoumanet system

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+# DynamoDB Table
+
+resouce "aws_dynamodb_table" "events" {
+    name         = "Events"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key     = "eventId"
+
+    attribute {
+        name = "eventId"
+        type = "S"
+    }
+}
+
+# SNS Topic
+
+resouce "aws_sns_topic" "event_role" {
+    name = "event_lambda_role"
+
+    assume_role_policy = jsonencode({
+        version = "2012-10-17"
+        Statement =[{
+            Action = "sts:AssumeRole"
+            Effect = "Allow"
+            Princpal {
+                service ="lambda.amazonaws.com
+            }
+        }]
+    })
+}
+
